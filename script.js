6fcb3d8954590377047774db28ce7c2527080b1d
sahil0sharm <143590016+sahil0sharm@users.noreply.github.com>
2023-08-31 16:43:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,12 +37,38 @@ document.querySelector("#india").addEventListener("click", function (e) {
 
 
 document.querySelector("#international").addEventListener("click", function (e) {
-    console.log(e.target.getAttribute("id"))
+    fetch("https://newsapi.org/v2/top-headlines&apiKey=dc61cf5d1b6e4d6e9042a01d34868313")
+        .then(responses => responses.json())
+        .then((news) => {
+            for (let i = 0; i < news.articles.length - 1; i++) {
+                newshtml += `<div class="ambani">
+            <img src="${(news.articles[i].urlToImage != null) ? news.articles[i].urlToImage : ""}" width="100%" height="225"/ >
+            <h2>${news.articles[i].title}</h2>
+            <p>${(news.articles[i].description != null) ? news.articles[i].description : ""}</p>
+            <p class="read"><a href="${news.articles[i].url}" target="_blank">READ MORE <i class="fa fa-long-arrow-right" aria-hidden="true"></a></i></p>
+        </div>`;
+            }
+            // console.log(newshtml)
+            king.innerHTML = newshtml;
+        })
 })
 
 
-document.querySelector("#sports").addEventListener("click", function (e) {
-    console.log(e.target.getAttribute("id"))
+document.querySelector("#technology").addEventListener("click", function (e) {
+    fetch("https://newsapi.org/v2/top-headlines?category=technology&apiKey=dc61cf5d1b6e4d6e9042a01d34868313")
+        .then(responses => responses.json())
+        .then((news) => {
+            for (let i = 0; i < news.articles.length - 1; i++) {
+                newshtml += `<div class="ambani">
+        <img src="${(news.articles[i].urlToImage != null) ? news.articles[i].urlToImage : ""}" width="100%" height="225"/ >
+        <h2>${news.articles[i].title}</h2>
+        <p>${(news.articles[i].description != null) ? news.articles[i].description : ""}</p>
+        <p class="read"><a href="${news.articles[i].url}" target="_blank">READ MORE <i class="fa fa-long-arrow-right" aria-hidden="true"></a></i></p>
+    </div>`;
+            }
+            // console.log(newshtml)
+            king.innerHTML = newshtml;
+        })
 })
 
 
